1222eafd93df895eba3d6387c06ac7fb1f535a7b
Yuvaraj <yuvarajyui27@gmail.com>
2025-01-04 22:16:19 +0530

    image and menu icon

--- src/Component/Navbar/index.tsx
+++ src/Component/Navbar/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import MenuSVG from '../../SVG/MenuSVG.tsx';
 
 const Navbar:React.FC = () => {
   return(
-    <nav className='flex-1'>
-      Nav
+    <nav className='h-7 px-2 flex items-center'>
+      <span className='justify-end button-hover' role='button' tabIndex={1}><MenuSVG /></span>
     </nav>
   );
 }
---
+++ src/SVG/MenuSVG.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+export default () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      <path d='M3 7H21' stroke="#f4f4f4" strokeWidth="1.5" strokeLinecap="round" />
+      <path d={isMenuOpen ? 'M3 12H18' : 'M3 12H21'} stroke="#f4f4f4" strokeWidth="1.5" strokeLinecap="round" strokeDasharray="24" strokeDashoffset={isMenuOpen ? 0 : 24} style={{ transition: 'stroke-dashoffset 0.5s ease', strokeDasharray: isMenuOpen ? '24' : '0' }} />
+      <path d='M3 17H21' stroke="#f4f4f4" strokeWidth="1.5" strokeLinecap="round"/>
+    </svg>
+  );
+}
